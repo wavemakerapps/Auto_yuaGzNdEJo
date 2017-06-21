7b94637dbddd0b1575e4770c2ad7f00504eef2ef
root <root@03244a0738c6>
2017-06-21 14:09:22 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_yuaGzNdEJo",
+  "homePage" : "Main",
+  "name" : "Auto_yuaGzNdEJo",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
